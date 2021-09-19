19a97580363e471b54625106f598883c12732352
Juliana Ifionu <julieifionu@gmail.com>
2021-09-19 17:47:04 +0100

    Render list in TodoContainer

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+/* eslint-disable react/prop-types */
+
+const TodoItem = (props) => {
+  const { todo } = props;
+  return (
+    <li>{todo.title}</li>
+  );
+};
+
+export default TodoItem;
---
+++ src/components/TodosList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+/* eslint-disable react/prop-types */
+
+const TodosList = (props) => {
+  const { todos } = props;
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
+};
+
+export default TodosList;

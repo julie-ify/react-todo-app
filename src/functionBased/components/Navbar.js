fe10c9297658883c6c9f896c40720585e7c3c638
Juliana Ifionu <julieifionu@gmail.com>
2021-09-20 19:31:08 +0100

    Add hamburger menu

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   const links = [
     {
       id: 1,
@@ -14,12 +17,33 @@ const Navbar = () => {
       text: 'About',
     },
   ];
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
+  const handleToggle = () => setNavbarOpen((prev) => !prev);
+
   return (
     <nav className="navBar">
-      <ul>
+      <button type="button" onClick={handleToggle}>
+        {navbarOpen ? (
+          <MdClose style={{ color: '#fff', width: '40px', height: '40px' }} />
+        ) : (
+          <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+        )}
+      </button>
+      <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
         {links.map((link) => (
           <li key={link.id}>
-            <NavLink to={link.path} activeClassName="active-link">{link.text}</NavLink>
+            <NavLink
+              to={link.path}
+              activeClassName="active-link"
+              onClick={() => closeMenu()}
+              exact
+            >
+              {link.text}
+            </NavLink>
           </li>
         ))}
       </ul>

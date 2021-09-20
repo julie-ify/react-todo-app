1276c6a5f3951f761137500bc54b8ebacaa1f618
Juliana Ifionu <julieifionu@gmail.com>
2021-09-20 19:47:19 +0100

    Fix Router deploy issue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './functionBased/App.css';
 import TodoContainer from './functionBased/components/TodoContainer';
 
 ReactDom.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <TodoContainer />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
